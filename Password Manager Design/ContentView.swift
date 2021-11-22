34963174ac63dd1ada8f57de74ecfe78c905cb2a
LuckDuracell <lukedru@yahoo.com>
2021-11-22 15:37:49 -0800

    Initial Commit

---
+++ Password Manager Design/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Password Manager Design
+//
+//  Created by Luke Drushell on 11/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Password Manager Design/Password_Manager_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Password_Manager_DesignApp.swift
+//  Password Manager Design
+//
+//  Created by Luke Drushell on 11/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Password_Manager_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
